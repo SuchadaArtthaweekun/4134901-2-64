2cc0f6914019f317b54345fcfee7416c18e39d47
SuchadaArtthaweekun <59986479+SuchadaArtthaweekun@users.noreply.github.com>
2022-03-05 14:49:24 +0700

    Add files via upload

---
+++ FinalLab/backend/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"net/http"
+
+	api "finallab/backend/api"
+
+	"github.com/gin-gonic/contrib/cors"
+	"github.com/gin-gonic/gin"
+)
+
+func indexView(c *gin.Context) {
+	c.Header("Access-Control-Allow-Origin", "*")
+	c.Header("Access-Control-Allow-Headers", "access-control-allow-origin, access-control-allow-headers")
+	c.JSON(http.StatusOK, gin.H{"message": "TODO APP"})
+}
+
+func SetupRoutes() *gin.Engine {
+	// Use Gin as router
+	router := gin.Default()
+	config := cors.DefaultConfig()
+	config.AllowAllOrigins = true
+	router.Use(cors.New(config))
+
+	// Set route for index
+	router.GET("/", indexView)
+	router.GET("/items", api.TodoItems)
+	router.GET("/item/del/:id", api.DeleteTodoItem)
+	router.GET("/item/add/:item", api.CreateTodoItem)
+	router.GET("/item/upd/:id/:done", api.UpdateTodoItem)
+
+	return router
+}
+
+func main() {
+	api.SetupPostgres()
+	router := SetupRoutes()
+	router.Run(":8081")
+}
